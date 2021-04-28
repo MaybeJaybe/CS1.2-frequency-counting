d42cc98cbab72da39d7a3e05a7052b771bfeefc2
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2021-04-27 22:34:44 -0700

    it works

--- HashTable.py
+++ HashTable.py
@@ -6,40 +6,46 @@ class HashTable:
     self.size = size
     self.arr = self.create_arr(size)
 
-
   # 1️⃣ TODO: Complete the create_arr method.
-
   # Each element of the hash table (arr) is a linked list.
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
 
   def create_arr(self, size):
-     
-    pass
-
-
-
+    array = []
+    for i in range(size):
+      array.append(LinkedList())
+    return array
 
   # 2️⃣ TODO: Create your own hash function.
-
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
 
-  def hash_func(self, key):
-    
-    pass
 
+# change h to num and see if program still works
+
+
+
+  def hash_func(self, key):
+    index = 1
+    for char in key:
+      index = index * 17
+    return index % self.size
 
   # 3️⃣ TODO: Complete the insert method.
-
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
-    pass
+    data = (key, value)
+    arr_index = self.hash_func(key)
+    linked_list = self.arr[arr_index]
+    linked_list_index = linked_list.find(key)
 
-
-
+    if linked_list_index == -1:
+      linked_list.append(data)
+    else:
+      linked_list.update(key)
+  # possibly needs double parentheses 
 
   # 4️⃣ TODO: Complete the print_key_values method.
-
   # Traverse through the every Linked List in the table and print the key value pairs.
 
   # For example: 
@@ -50,7 +56,5 @@ class HashTable:
   # erase: 2
 
   def print_key_values(self):
-    pass
-
-
-
+    for LinkedList in self.arr:
+      LinkedList.print_nodes()
